89e58d68fa1c199dc82fe0fe31a5564baa787903
Bryant Milliren <kbmilliren@northstate.net>
2015-04-02 18:47:58 -0400

    First Commit

---
+++ CarFinderAPI/CarFinderAPI/Controllers/CarsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Insight.Database;
+using Microsoft.AspNet.Identity.Owin;
+using CarFinderAPI.Models.Data_Interfaces;
+using System.Web;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace CarFinderAPI.Controllers
+{
+    [RoutePrefix("api/cars")]
+    public class CarsController : ApiController
+    {
+        private ICarsAccess db;
+
+        public CarsController()
+        {
+            db = HttpContext.Current.GetOwinContext().Get<SqlConnection>().As<ICarsAccess>();
+        }
+        [HttpGet, Route("getCars")]
+        public IHttpActionResult getCars()
+        {
+            return Ok("Its a car!");
+        }
+
+        [HttpGet, HttpPost, Route("getYears")]
+        public async Task<IHttpActionResult> GetYears()
+        {
+            return Ok(await db.GetYears());
+        }
+
+        [HttpGet, HttpPost, Route("getMakes")]
+        public async Task<IHttpActionResult> GetMakes(int year)
+        {
+            return Ok(await db.GetMakes(year));
+        }
+
+        [HttpGet, HttpPost, Route("getModels")]
+        public async Task<IHttpActionResult> GetModels(int year, string make)
+        {
+            return Ok(await db.GetModels(year, make));
+        }
+
+        [HttpGet, HttpPost, Route("getTrims")]
+        public async Task<IHttpActionResult> GetTrims(int year, string make, string model)
+        {
+            return Ok(await db.GetTrims(year, make, model));
+        }
+
+
+        [HttpGet, HttpPost, Route("getCars")]
+        public async Task<IHttpActionResult> GetCars(int year, string make, string model, string trim)
+        {
+            return Ok(await db.GetCars(year, make, model, trim));
+        }
+        
+    }
+}
---
+++ CarFinderAPI/CarFinderAPI/Models/Car.cs
@@ -0,0 +1,20 @@
+﻿using Insight.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarFinderAPI.Models
+{
+    public class Car
+    {
+        [Column("model_year")]
+        public int year { get; set; }
+        public string make { get; set; }
+        [Column("model_name")]
+        public string model { get; set; }
+        [Column("model_trim")]
+        public string trim { get; set; }
+        public int id { get; set; }
+    }
+}
